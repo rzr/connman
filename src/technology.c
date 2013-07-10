c94f440d0d499e6307dc226787927d19eff31a5a
Chengyi Zhao <chengyi1.zhao@archermind.com>
2013-07-10 20:00:36 +0800

    Tethering: Notify listeners when station connection changes

--- src/technology.c
+++ src/technology.c
@@ -1057,6 +1057,10 @@ static const GDBusMethodTable technology_methods[] = {
 static const GDBusSignalTable technology_signals[] = {
 	{ GDBUS_SIGNAL("PropertyChanged",
 			GDBUS_ARGS({ "name", "s" }, { "value", "v" })) },
+	{ GDBUS_SIGNAL("DhcpConnected",
+			GDBUS_ARGS({ "name", "s" })) },
+	{ GDBUS_SIGNAL("DhcpLeaseDeleted",
+			GDBUS_ARGS({ "name", "s" })) },
 	{ },
 };
 
