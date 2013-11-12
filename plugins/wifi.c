dc7bc4ee09b70da32213368edb8503901cd60941
Michael Ikey Doherty <michael.i.doherty@intel.com>
2013-11-12 09:46:50 +0000

    wifi: Ensure that scan_params is initialized
    Background scanning is never correctly initialized as scan_params has
    never been initialized, and the current behaviour is to allocate the
    wifi scan_params if it is null.
    
    This commit fixes background scanning by ensuring we set the pointer
    to NULL.
    
    Bug-Tz: TZPC-2589
    
    Change-Id: Ifdbf0910523327ff325e9b17bceb0e389821bd50
    Signed-off-by: Michael Ikey Doherty <michael.i.doherty@intel.com>

--- plugins/wifi.c
+++ plugins/wifi.c
@@ -176,6 +176,13 @@ static int wifi_probe(struct connman_device *device)
 	if (!wifi)
 		return -ENOMEM;
 
+	wifi->connected = false;
+	wifi->disconnecting = false;
+	wifi->tethering = false;
+	wifi->bridged = false;
+	wifi->bridge = NULL;
+	wifi->scan_params = NULL;
+
 	wifi->state = G_SUPPLICANT_STATE_INACTIVE;
 
 	connman_device_set_data(device, wifi);
